8ae1839238e2db95054236bd2144fa3aade28d3e
juzhiyuan <juzhiyuan@apache.org>
2020-09-13 19:35:28 +0800

    feat: added i18n todo

--- packages/plugin/src/PluginCard.tsx
+++ packages/plugin/src/PluginCard.tsx
@@ -9,6 +9,7 @@ interface Props extends CardProps {
 const PluginCard: React.FC<Props> = ({ name, actions }) => {
   return (
     <Card actions={actions}>
+      {/* TODO: https://github.com/ant-design/pro-components/pull/379/files#diff-9b2c55deb25c2f8fec0e59c7bf59ce4aR75 */}
       <Card.Meta title={name} description="暂无简介" />
     </Card>
   );
